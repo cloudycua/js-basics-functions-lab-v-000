67de839c31b49f775cc0ad0c82fc8f7307411c28
Christine Cua <cloudycua@gmail.com>
2019-02-08 15:29:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,3 @@ function distanceTravelledInFeet(pickup, dropoff) {
 }
 
 
-// calculate the number of feet travelled based on the distance. Write a function called distanceTravelledInFeet that takes in the beginning and destination blocks and returns the number of feet travelled. Each block in Manhattan is 264 feet long. For example distanceTravelledInFeet(34, 38) representing 34th St to 38th St, returns 1056 ((38-34)*264). You can assume that we are only calculating distance uptown/downtown, not from river to river.
